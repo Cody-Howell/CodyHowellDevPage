a0f15ebbe3822d8506e5fb29b849b31ced61de25
Cody-Howell <codyhowell44@gmail.com>
2024-05-14 10:58:39 -0600

    Adjusted page

--- devhomepage/src/App.js
+++ devhomepage/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './scsscomp/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dev Homepage</h1>
+        <p>This is a temporary page for Cody's developer homepage. It'll have a few links for all my current web projects. 
+          Come back later!</p>
+      </div>
+    );
+  }
 }
 
 export default App;
